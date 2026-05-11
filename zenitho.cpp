a124c302557691e18265ad466305bbc392917a4b
Bahlil_Kader <mzenitho.mp@gmail.com>
2026-05-12 01:59:48 +0700

    Create zenitho.cpp

---
+++ zenitho.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct siswa {
+    string nama;
+    int umur;
+    int kelas;
+};
+
+void rekursif (int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << n << " ";
+    rekurisif(n - 1);
+}
+
+int main (){
+    siswa zunit[324];
+    int pilihan;
+
+
+    do
+    {
+
+
+
+
+
+
+
+    } while (pilihan == 1 || pilihan == 2 || pilihan == 3);
+
+
+
+
+
+}
